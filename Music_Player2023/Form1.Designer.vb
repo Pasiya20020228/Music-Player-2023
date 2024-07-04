19f6ad2de51d757886de574ef51d03270cde054e
Pasiya20020228 <madumalbct001@gmailcom>
2024-07-04 22:44:41 +0530

    First commit and upload files

---
+++ Music_Player2023/Form1.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ThemesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VersionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ActivateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.stopbtn = New System.Windows.Forms.Button()
+        Me.playbtn = New System.Windows.Forms.Button()
+        Me.deletebtn = New System.Windows.Forms.Button()
+        Me.selectbtn = New System.Windows.Forms.Button()
+        Me.addbtn = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.op = New System.Windows.Forms.OpenFileDialog()
+        Me.Player1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.MenuStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.Player1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1039, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(125, 26)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(125, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SettingsToolStripMenuItem, Me.ThemesToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(47, 24)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(137, 26)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'ThemesToolStripMenuItem
+        '
+        Me.ThemesToolStripMenuItem.Name = "ThemesToolStripMenuItem"
+        Me.ThemesToolStripMenuItem.Size = New System.Drawing.Size(137, 26)
+        Me.ThemesToolStripMenuItem.Text = "Themes"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.VersionToolStripMenuItem, Me.ActivateToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(53, 24)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'VersionToolStripMenuItem
+        '
+        Me.VersionToolStripMenuItem.Name = "VersionToolStripMenuItem"
+        Me.VersionToolStripMenuItem.Size = New System.Drawing.Size(138, 26)
+        Me.VersionToolStripMenuItem.Text = "Version"
+        '
+        'ActivateToolStripMenuItem
+        '
+        Me.ActivateToolStripMenuItem.Name = "ActivateToolStripMenuItem"
+        Me.ActivateToolStripMenuItem.Size = New System.Drawing.Size(138, 26)
+        Me.ActivateToolStripMenuItem.Text = "Activate"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.stopbtn)
+        Me.Panel1.Controls.Add(Me.deletebtn)
+        Me.Panel1.Controls.Add(Me.selectbtn)
+        Me.Panel1.Controls.Add(Me.addbtn)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Location = New System.Drawing.Point(0, 31)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1039, 480)
+        Me.Panel1.TabIndex = 1
+        '
+        'stopbtn
+        '
+        Me.stopbtn.Image = CType(resources.GetObject("stopbtn.Image"), System.Drawing.Image)
+        Me.stopbtn.Location = New System.Drawing.Point(932, 508)
+        Me.stopbtn.Name = "stopbtn"
+        Me.stopbtn.Size = New System.Drawing.Size(75, 71)
+        Me.stopbtn.TabIndex = 6
+        Me.stopbtn.UseVisualStyleBackColor = True
+        Me.stopbtn.Visible = False
+        '
+        'playbtn
+        '
+        Me.playbtn.Image = CType(resources.GetObject("playbtn.Image"), System.Drawing.Image)
+        Me.playbtn.Location = New System.Drawing.Point(187, 529)
+        Me.playbtn.Name = "playbtn"
+        Me.playbtn.Size = New System.Drawing.Size(75, 71)
+        Me.playbtn.TabIndex = 5
+        Me.playbtn.UseVisualStyleBackColor = True
+        '
+        'deletebtn
+        '
+        Me.deletebtn.Image = CType(resources.GetObject("deletebtn.Image"), System.Drawing.Image)
+        Me.deletebtn.Location = New System.Drawing.Point(61, 293)
+        Me.deletebtn.Name = "deletebtn"
+        Me.deletebtn.Size = New System.Drawing.Size(75, 71)
+        Me.deletebtn.TabIndex = 3
+        Me.deletebtn.UseVisualStyleBackColor = True
+        '
+        'selectbtn
+        '
+        Me.selectbtn.Image = CType(resources.GetObject("selectbtn.Image"), System.Drawing.Image)
+        Me.selectbtn.Location = New System.Drawing.Point(61, 84)
+        Me.selectbtn.Name = "selectbtn"
+        Me.selectbtn.Size = New System.Drawing.Size(75, 71)
+        Me.selectbtn.TabIndex = 2
+        Me.selectbtn.UseVisualStyleBackColor = True
+        '
+        'addbtn
+        '
+        Me.addbtn.Image = CType(resources.GetObject("addbtn.Image"), System.Drawing.Image)
+        Me.addbtn.Location = New System.Drawing.Point(61, 184)
+        Me.addbtn.Name = "addbtn"
+        Me.addbtn.Size = New System.Drawing.Size(75, 71)
+        Me.addbtn.TabIndex = 1
+        Me.addbtn.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(178, 20)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(829, 452)
+        Me.ListBox1.TabIndex = 0
+        '
+        'op
+        '
+        Me.op.FileName = "OpenFileDialog1"
+        '
+        'Player1
+        '
+        Me.Player1.Enabled = True
+        Me.Player1.Location = New System.Drawing.Point(358, 539)
+        Me.Player1.Name = "Player1"
+        Me.Player1.OcxState = CType(resources.GetObject("Player1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.Player1.Size = New System.Drawing.Size(257, 69)
+        Me.Player1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1039, 632)
+        Me.Controls.Add(Me.Player1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.playbtn)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Music_Player 2023"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.Player1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ThemesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VersionToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ActivateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents deletebtn As Button
+    Friend WithEvents selectbtn As Button
+    Friend WithEvents addbtn As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents op As OpenFileDialog
+    Friend WithEvents stopbtn As Button
+    Friend WithEvents playbtn As Button
+    Friend WithEvents Player1 As AxWMPLib.AxWindowsMediaPlayer
+End Class
---
+++ Music_Player2023/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Dim x As String
+    Dim filename As String
+    Dim c As Integer = 0
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        x = MsgBox("Are you sure to exit?", vbYesNo, "NOTICE")
+        If x = MsgBoxResult.Yes Then
+            Close()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles addbtn.Click
+        op.ShowDialog()
+        If op.FileName <> "" Then
+            filename = op.FileName
+            ListBox1.Items.Add(filename)
+        End If
+        'Update variable to count
+        c = c + 1
+    End Sub
+
+    Private Sub selectbtn_Click(sender As Object, e As EventArgs) Handles selectbtn.Click
+        If c > 0 Then
+            ListBox1.SelectedIndex = 0
+        End If
+
+    End Sub
+
+    Private Sub deletebtn_Click(sender As Object, e As EventArgs) Handles deletebtn.Click
+        c = c - 1
+        If c > 0 Then
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+        End If
+    End Sub
+
+    Private Sub playbtn_Click(sender As Object, e As EventArgs) Handles playbtn.Click
+        If c > 0 Then
+            Player1.URL = ListBox1.SelectedItem
+        End If
+    End Sub
+
+    Private Sub stopbtn_Click(sender As Object, e As EventArgs) Handles stopbtn.Click
+
+    End Sub
+
+    Private Sub VersionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VersionToolStripMenuItem.Click
+        MsgBox("This is 0.1.1 Version", vbOKOnly, "NOTICE")
+    End Sub
+End Class
